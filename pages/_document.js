c8d407beb9d6540e44a7b830ee87914524a20a84
aayofujiwara-tech <aa.yo.fujiwara@gmail.com>
2026-06-18 13:26:54 +0900

    feat: OGP画像(SVG)・メタタグを追加

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,23 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Syne:wght@700;800&family=DM+Sans:ital,opsz,wght@0,9..40,400;0,9..40,500&display=swap"
           rel="stylesheet"
         />
+        {/* OGP */}
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="DMM Renamer" />
+        <meta property="og:title" content="DMM Renamer - DMMファイルを自動でリネーム" />
+        <meta property="og:description" content="DMMでダウンロードした.dcvファイルを女優名・タイトルで自動リネーム。貼り付けるだけ、無料、登録不要。" />
+        <meta property="og:url" content="https://dmm-rename-web.vercel.app" />
+        <meta property="og:image" content="https://dmm-rename-web.vercel.app/ogp.svg" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta property="og:locale" content="ja_JP" />
+        {/* Twitter Card */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="DMM Renamer - DMMファイルを自動でリネーム" />
+        <meta name="twitter:description" content="DMMでダウンロードした.dcvファイルを女優名・タイトルで自動リネーム。貼り付けるだけ、無料、登録不要。" />
+        <meta name="twitter:image" content="https://dmm-rename-web.vercel.app/ogp.svg" />
+        {/* description */}
+        <meta name="description" content="DMMでダウンロードした.dcvファイルを女優名・タイトルで自動リネーム。貼り付けるだけ、無料、登録不要。" />
       </Head>
       <body>
         <Main />
